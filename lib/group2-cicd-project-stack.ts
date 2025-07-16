ce6870c5d0e5f17c41453f1461d8d4a04d4bbc1e
Aicha-Fadel <a.acihafadel@gmail.com>
2025-07-16 17:09:07 +0000

    Initial commit

---
+++ lib/group2-cicd-project-stack.ts
@@ -0,0 +1,16 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class Group2CicdProjectStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'Group2CicdProjectQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+  }
+}
